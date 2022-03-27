4bb34d65f9d01158e39e9145ae1c07c176600ec9
JINHEE LEE <ms.jinhee.lee@gmail.com>
2022-03-27 16:48:32 +1100

    update 2

--- Assets/script.js
+++ Assets/script.js
@@ -2,32 +2,29 @@
  * CRITERIA
  */
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
+//  GIVEN I am using a daily planner to create a schedule
+//  WHEN I open the planner
+//  1. THEN the current day is displayed at the top of the calendar
+//  WHEN I scroll down
+//  THEN I am presented with timeblocks for standard business hours
+//  WHEN I view the timeblocks for that day
+//  THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//  WHEN I click into a timeblock
+//  THEN I can enter an event
+//  WHEN I click the save button for that timeblock
+//  THEN the text for that event is saved in local storage
+//  WHEN I refresh the page
+//  THEN the saved events persist
 /**
  * DEFINE VARIABLES
  */
-
  var saveBtn = $(".saveBtn");
-
  /**
   * FUNCTIONS
   */
  
  // current day is displayed at the top of the calendar
- $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+ $("#currentDay").text(moment().format('dddd Do of MMMM YYYY'));
  
  // each time block is color-coded to indicate whether it is in the past, present, or future
  function timeBlockColor() {
@@ -79,7 +76,6 @@
  /**
   * CALL FUNCTIONS
   */
- 
  timeBlockColor();
  usePlanner();
  
